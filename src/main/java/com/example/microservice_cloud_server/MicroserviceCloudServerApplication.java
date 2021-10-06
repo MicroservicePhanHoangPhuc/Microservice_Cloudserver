edef6e7ec759bb2ff07499590d15b098bce9bbaf
phanphuc2804 <phanphuc28042000@gmail.com>
2021-10-06 12:01:34 +0700

    Add Microservice_Cloudserver

---
+++ src/main/java/com/example/microservice_cloud_server/MicroserviceCloudServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservice_cloud_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCloudServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceCloudServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/microservice_cloud_server/MicroserviceCloudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservice_cloud_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCloudServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
